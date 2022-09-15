a4440a1ecc4e9593e921e887203859dfb1b0eb21
Achoycito <56978465+Achoycito@users.noreply.github.com>
2022-09-15 10:03:57 -0600

    Primer trabajo con less, form login basico

---
+++ U4/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <link rel="stylesheet" href="public/css/main.css">
+    
+</head>
+<body>
+    <div class="container">
+        <form>
+            <fieldset>
+                <legend>Inicio de sesion</legend>
+
+                <label>Email</label>
+                <input type="text" name="">
+    
+                <label>Contraseña</label>
+                <input type="password" name="">
+
+                <button type="submit">Acceder</button>
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
