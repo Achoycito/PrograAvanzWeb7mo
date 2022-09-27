bc00190fd588907819c3d9f9fef068dd443744b9
Achoycito <56978465+Achoycito@users.noreply.github.com>
2022-09-27 13:49:28 -0600

    Ahora si funciona porque le cambie https a http GRACIAS

--- U4/Clase3/app/AuthController.php
+++ U4/Clase3/app/AuthController.php
@@ -1,16 +1,15 @@
 <?php
 
-// if(isset($_POST['action'])){
-//     switch($_POST['action']){
-//         case 'access':
-//             $authController = new AuthController();
-//             $email = strip_tags($_POST['email']);
-//             $password = strip_tags($_POST['password']);
-//     }
-// }
-
-$authC = new AuthController();
-$authC->login('','');
+if(isset($_POST['action'])){
+    switch($_POST['action']){
+        case 'access':
+            $authController = new AuthController();
+            $email = strip_tags($_POST['email']);
+            $password = strip_tags($_POST['password']);
+            $authController->login($email, $password);
+        break;
+    }
+}
 
 Class AuthController{
     public function login($email, $password){
@@ -18,7 +17,7 @@ Class AuthController{
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+        CURLOPT_URL => 'http://crud.jonathansoto.mx/api/login',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -26,17 +25,26 @@ Class AuthController{
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => array('email' => 'agonzalez2_19@alu.uabcs.mx','password' => '2b2Aw@6UIf34ZY'),
+        CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
         ));
 
         $response = curl_exec($curl);
         curl_close($curl);
+        echo $response;
+
         $response = json_decode($response);
 
-        echo $response;
-        
-        // var_dump($response);
-        // echo 'Hola';
+        if(isset($response->code) && $response->code > 0){
+            session_start();
+            $_SESSION["name"] = $response->data->name;
+            $_SESSION["lastname"] = $response->data->lastname;
+            $_SESSION["avatar"] = $response->data->avatar;
+            $_SESSION["token"] = $response->data->token;
+            header("Location:../products");
+        }
+        else{
+            header("Location:../?error=true");
+        }
     }
 }
 
--- U4/Clase3/index.php
+++ U4/Clase3/index.php
@@ -27,15 +27,16 @@
                     <h3>Bienvenido a <br>Pollito Pio Producciones</h3>
                     <p>Inicie sesion para la oportunidad de su vida.</p>
                 </div>
-                <form action="products" method="get" class="form">
+                <form action="app/AuthController.php" method="post" class="form">
                     <div class="mb-3">
                         <label for="email">Correo electronico</label>
-                        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                        <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
                     </div>
                     <div>
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Contraseña">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Contraseña">
                     </div>
+                    <input type="hidden" name="action" value="access">
                     <button class="btn btn-primary" type="submit">Acceder</button>
                 </form>
             </div>
