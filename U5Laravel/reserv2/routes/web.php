6a1023dd4c8ae5cf9bff8e2640e4c1d00356542e
Achoycito <56978465+Achoycito@users.noreply.github.com>
2022-10-20 00:22:59 -0600

    Proyecto Laravel ahora si con XAMPP y con PHP y Laravel actulizados VAMOS A POR TODASSSS

---
+++ U5Laravel/reserv2/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Saludo
+Route::get('/saludo', function () {
+    echo "<h1>Hola</h1>";
+});
+
+// Saludo que recibe variable nombre
+Route::get('/saludo/{name}', function ($name) {
+    echo "<h1>Hola {$name} </h1>";
+});
+
+// Suma pero el tercer numero es opcional
+Route::get('/suma/{n1}/{n2}/{n3?}', function ($n1, $n2, $n3=0) {
+    echo "<h1>".($n1 + $n2 + $n3)."</h1>";
+})->where(['n1' => '[0-9]+'], ['n2' =>'[0-9]+']);
+
+Route::post('suma/', function(Request $request){});
+
+Route::get('users/', [UserController::class, 'index']);
+Route::get('users/create', [UserController::class, 'create']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::post('users/', [UserController::class, 'store']);
